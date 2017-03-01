444c296106b8f3e8d507c0aac70f3aa023b3190f
Ah Lim Om <ahlim0m@hawaii.edu>
2017-02-28 14:45:19 -1000

    Task 5: Events, Sessions

--- app/leaderboard.js
+++ app/leaderboard.js
@@ -1,1 +1,30 @@
-console.log("Hello world");
+PlayersList = new Mongo.Collection('players');
+
+if(Meteor.isClient){
+  Template.leaderboard.player = function(){
+    return "Some other text";
+  },
+
+  Template.leaderboard.helpers({
+    'player': function(){
+      return PlayersList.find();
+    },
+    'selectedClass': function(){
+      var playerId = this._id;
+      var selectedPlayer = Session.get('selectedPlayer');
+      if(playerId == selectedPlayer){
+        return "selected"
+      }
+    }
+  });
+
+  Template.leaderboard.events({
+    'click .player': function(){
+      var playerId = this._id;
+      Session.set('selectedPlayer', playerId);
+    }
+  });
+}
+
+if(Meteor.isServer){
+}
